c4f81a733da71be6a2687fcaa0b052bf81e294c8
Asakout <achraf.sakout@capgemini.com>
2023-01-16 09:36:26 +0100

    init project

---
+++ src/main/java/ma/sellPoint/PointDeVente/PointDeVenteApplication.java
@@ -0,0 +1,13 @@
+package ma.sellPoint.PointDeVente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointDeVenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointDeVenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/sellPoint/PointDeVente/PointDeVenteApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.sellPoint.PointDeVente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointDeVenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
